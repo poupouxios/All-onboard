669c52d76ba6ee74db79f013fd9ef41dba4b330e
Valentinos <valentinos@llamadigital.net>
2015-10-24 13:03:01 +0100

    Initial Commit

---
+++ All-onboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  All-onboard
+//
+//  Created by Valentinos Papasavvas on 24/10/2015.
+//  Copyright © 2015 Llamadigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
